002bd2d159de14d56aa9829d57f443b25de44990
simplydesigner <simplydesigner@ya.ru>
2016-08-23 17:34:15 +0300

    Initial Commit

---
+++ r_l_values_refernces/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  r_l_values_refernces
+//
+//  Created by simplydesigner on 8/23/16.
+//  Copyright © 2016 simplydesigner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
